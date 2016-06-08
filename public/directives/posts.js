9fee1457db480fbcb9d91c39054357251ac70c2b
Joshua <joshuamajebi@yahoo.com>
2016-06-08 13:56:33 +0100

    test change

--- public/directives/posts.js
+++ public/directives/posts.js
@@ -159,6 +159,8 @@ myApp.directive('postItem', ['$compile', '$state', '$sce', 'service', 'responseR
             $state.go('user', {id: uid});
         }
         
+		console.log(scope.content);
+		
         var incrementStorageShare = function(){
             var current_posts = storageService.getStorageValue("posts");
                                
